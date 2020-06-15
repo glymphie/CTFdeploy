4f21315a8f90ec8e0c8426f7ae64d961c5291362
Glymph <peterhusted@hotmail.com>
2020-06-16 00:21:14 +0200

    made timezone.py more readable

--- OCD/CTFd_setup/timezone.py
+++ OCD/CTFd_setup/timezone.py
@@ -3,11 +3,11 @@ Calculates the local time difference and accounts for daylight saving
 """
 import time
 
-t = list(time.strftime("%z"))
+t = time.strftime("%z")
 
 if t[0] == '+':
-    utcdiff = (int(t[1]) * 36000 + (int(t[2]) - time.localtime().tm_isdst) * 3600 + int(t[3]) * 600 + int(t[4]) * 60)
+    utcdiff = ((int(t[1:3]) - time.localtime().tm_isdst) * 3600 + int(t[3:5]) * 60)
     print('+' + str(utcdiff))
 else:
-    utcdiff = (int(t[1]) * 36000 + (int(t[2]) + time.localtime().tm_isdst) * 3600 + int(t[3]) * 600 + int(t[4]) * 60)
+    utcdiff = ((int(t[1:3]) + time.localtime().tm_isdst) * 3600 + int(t[3:5]) * 60)
     print('-' + str(utcdiff))
